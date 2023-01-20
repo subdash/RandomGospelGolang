8350085b512b92957314b5e4c774ba83f3c67e60
Dashiell Sublett <dhrs.dev@gmail.com>
2023-01-20 16:23:48 -0600

    * Fix main

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"random-gospel/lib"
 )
 
 func main() {
 	request := lib.RandomGospel()
-	resp := lib.GetVerse(&request)
+	resp, err := lib.GetVerse(&request)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
 	fmt.Println(<-resp)
 }
