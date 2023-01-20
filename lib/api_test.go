26b8ee97c1fd43e7056d464ec159e371cb6a7b50
Dashiell Sublett <dhrs.dev@gmail.com>
2023-01-20 14:34:31 -0600

    * Add coverage

--- lib/api_test.go
+++ lib/api_test.go
@@ -5,6 +5,28 @@ import (
 	"testing"
 )
 
+func Test_BibleApiRequest_stringer(t *testing.T) {
+	mockRequest := BibleApiRequest{
+		Book: John,
+		Chapter: 3,
+		Verses: VerseRange{
+			Begin: 1,
+			End: 3,
+		},
+	}
+	expected := fmt.Sprintf(
+		"Book: %s, Chapter: %d, Verses: %d, %d",
+		John,
+		3,
+		1,
+		3)
+
+	if asString := fmt.Sprintf("%s", mockRequest); asString != expected {
+		t.Fatalf(`Unexpected text: %s`, asString)
+	}
+}
+
+
 func Test_bibleApiResponse_stringer(t *testing.T) {
 	mockReference := "John 4:4-11"
 	mockText := "And he must needs go through Samaria...(truncated)"
